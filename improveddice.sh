4b85164bfb2eb136ac12b5d440c99f22896f6e0b
jatinder singh <jatinder18>
2023-06-30 00:15:24 -0400

    lab 2

---
+++ improveddice.sh
@@ -0,0 +1,11 @@
+--2023-06-29 22:51:13--  https://zonzorp.github.io/COMP2101/Labs/bash/scripts-lab2/improveddice.sh
+Resolving zonzorp.github.io (zonzorp.github.io)... 185.199.109.153, 185.199.110.153, 185.199.111.153, ...
+Connecting to zonzorp.github.io (zonzorp.github.io)|185.199.109.153|:443... connected.
+HTTP request sent, awaiting response... 200 OK
+Length: 723 [application/x-sh]
+Saving to: ‘improveddice.sh’
+
+     0K                                                       100% 68.3M=0s
+
+2023-06-29 22:51:13 (68.3 MB/s) - ‘improveddice.sh’ saved [723/723]
+
